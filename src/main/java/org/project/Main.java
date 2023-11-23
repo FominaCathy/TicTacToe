87c18cf0b65f70aad7274d6ca000d3aed227112d
FominaCathy <99419802+FominaCathy@users.noreply.github.com>
2023-11-23 12:56:26 +0400

    игра крестики -нолики. 4й вариант. корректировка и ограничения на размер игры и кол-ва фишек для победы

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -5,7 +5,7 @@ import org.project.TicTacGame.ConsoleUI;
 public class Main {
 
     public static void main(String[] args) {
-        int sizeField = 2;
+        int sizeField = 5;
         int winCount = 4;
         ConsoleUI.game(sizeField, winCount);
 
